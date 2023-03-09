0af53a87e5390939d135badc0411238d9fd74855
joel <joeldavethu@gmail.com>
2023-03-09 20:17:42 +0800

    initial message

---
+++ Topics/Printing data/Theory/src/Task.java
@@ -0,0 +1,31 @@
+// You can experiment here, it won’t be checked
+
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+
+//    System.out.println("I ");
+//    System.out.println("know ");
+//    System.out.println("Java ");
+//    System.out.println("well.");
+
+//    System.out.println("Java is a popular programming language");
+//    System.out.println();
+//    System.out.println("It is used all over the world!");
+
+//    System.out.print("I ");
+//    System.out.print("know ");
+//    System.out.print("Java ");
+//    System.out.println("well.");
+
+    System.out.print(108);
+    System.out.print('c');
+    System.out.print("Q");
+    System.out.println('3');
+
+    System.out.print(22);
+    System.out.print('E');
+    System.out.print(8);
+    System.out.println('1');
+  }
+}
